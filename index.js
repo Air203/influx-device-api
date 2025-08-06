a7b883920899116f74fe295f1ac5da7c63b91c8d
Air203 <zoeyy738@gmail.com>
2025-08-06 17:41:14 +0700

    Update commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { getDevices } from './_devices.js'; // đường dẫn đúng với file bạn vừa gửi
+
+const run = async () => {
+  try {
+    const data = await getDevices();
+    if (data.length === 0) {
+      console.log("⚠️ Không có dữ liệu nào được tìm thấy.");
+    } else {
+      console.log("✅ Dữ liệu truy vấn từ InfluxDB:");
+      console.log(JSON.stringify(data, null, 2)); // In đẹp
+    }
+  } catch (err) {
+    console.error("❌ Lỗi khi truy vấn InfluxDB:", err.message || err);
+  }
+};
+
+run();
